0cfb4cd23926d5b2d7ea9990d61de31260c20209
modext <chigomorji@gmail.com>
2023-03-30 08:28:04 +0100

    g

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "Ther person has passed and the mark is"+ str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "Ther person has failed and the mark is "+ str(score)
+
+@app.route('/result/<int:marks>')
+def result(marks):
+    result = ""
+    if marks<50:
+        result="fail"
+    else:
+        result= 'success'
+    return redirect(url_for(result,score=marks))
+
+@app.route('/men/')
+def men_page():
+    return render_template('men.html')
+    # return render_template('men.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
